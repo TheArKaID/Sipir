3dc8b129f5fc5fb3d32aaadbb4dd76bc4050ed21
Arifia Kasastra R <arka.progammer@gmail.com>
2020-12-09 14:53:37 +0700

    Add Main Program View

--- SistemPencatatanParkir/Program.cs
+++ SistemPencatatanParkir/Program.cs
@@ -3,13 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace SistemPencatatanParkir
 {
     class Program
     {
+        List<Mahasiswa> allMahasiswa = new List<Mahasiswa>();
+        List<Kendaraan> allKendaraan = new List<Kendaraan>();
+
         static void Main(string[] args)
         {
+            Program program = new Program();
+
+            bool again = true;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Sistem Pencatan Parkir.\nAplikasi ini mencatat data Mahasiswa, Kendaraan dan Histori keluar masuknya.");
+                Console.WriteLine("1. Admin\n2. Mahasiswa\n0. Keluar");
+
+                if (!int.TryParse(Console.ReadLine(), out int key))
+                {
+                    Console.Clear();
+                    continue;
+                }
+
+                if (key == 1)
+                {
+                    // Admin
+                }
+                else if (key == 2)
+                {
+                    // Mahasiswa Section
+
+                }
+                else if (key == 0)
+                {
+                    again = false;
+                }
+                else
+                {
+                    Console.WriteLine("Tidak ada pilihan lain.");
+                }
+
+            } while (again);
+            Console.WriteLine("Closing app...");
+            Thread.Sleep(1500);
         }
     }
 }
